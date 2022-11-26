3fe3660bdd456f04b50e37cf360d2b45c348a640
Fabrício Guimarães <fabricio15.guimaraes@gmail.com>
2022-11-26 13:03:25 -0400

    Update Sala.java

--- LAB05/Sala.java
+++ LAB05/Sala.java
@@ -1,4 +1,4 @@
-public class Sala{
+public class Sala implements Comparable<Sala>{
     int bloco, sala, capacidade;
     boolean acessivel;
 
@@ -11,9 +11,26 @@ public class Sala{
         this.acessivel = acessivel;
     }
 
-
     String getDescricao(){
         if (this.acessivel==true) return (String.format("Bloco %d, Sala %d (%d lugares, acessível)", this.bloco, this.sala, this.capacidade));
         return (String.format("Bloco %d, Sala %d (%d lugares, não acessível)", this.bloco, this.sala, this.capacidade));
+    }
+
+    String getCapacidade(){
+        return String.format("Sala: %d, Capacidade: %d", this.sala, this.capacidade);
+    }
+
+    public int getCapacidadeInt(){
+        return this.capacidade;
+    }
+
+    @Override
+    public int compareTo(Sala outraSala){
+        if (this.capacidade < outraSala.getCapacidadeInt()){
+            return -1;
+        }else if(this.capacidade > outraSala.getCapacidadeInt()){
+            return 1;
+        }
+        return 0;
     }
 }
